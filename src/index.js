f379323f7b9f8700a5459ad3bd20d92b1a1a06b9
Andres Betancourt <aebb001@gmail.com>
2020-11-21 11:00:04 -0400

    fixing menu

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ const menu = document.getElementById("menu");
 const boton = document.querySelectorAll(".link");
 
 const openMenu = () => {
-  menu.classList.toggle("open");
+  if (menu.style.top === "0px") {
+    return (menu.style.top = "-30vh");
+  }
+
+  menu.style.top = 0;
+};
+
+const closeMenu = () => {
+  menu.style.top = "-30vh";
 };
 boton.forEach((btn) => {
   btn.addEventListener("click", openMenu);
